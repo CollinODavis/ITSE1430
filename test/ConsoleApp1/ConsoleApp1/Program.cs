ccaecd3849d2fe388495c0d19e80fea474f12458
CollinODavis <collin.davis590@my.tccd.edu>
2018-01-24 21:41:01 -0600

    Class

---
+++ test/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            int Name;
+
+            Console.WriteLine("enter your name: ");
+            Name = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("your name is " + Name);
+            Console.ReadLine();
+        }
+    }
+}
