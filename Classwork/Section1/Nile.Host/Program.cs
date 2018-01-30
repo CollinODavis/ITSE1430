6713b5579d5abb262df329f455f59d92533f7487
CollinODavis <collin.davis590@my.tccd.edu>
2018-01-29 21:39:06 -0600

    product program

--- Classwork/Section1/Nile.Host/Program.cs
+++ Classwork/Section1/Nile.Host/Program.cs
@@ -6,9 +6,111 @@ namespace Nile.Host
     {
         static void Main( string[] args )
         {
-           
+           bool quit = false;
+           while (!quit)
+            {
+                //SHOW MENU
+                char choice = DisplayMenu();
+
+
+                //PROCESS MENU OPTIONS
+                switch (choice)
+                {
+                    case 'l':
+                    case 'L': ListProducts(); break;
+                    case 'a':
+                    case 'A': AddProduct(); break;
+                    case 'q':
+                    case 'Q': quit = true; break;
+                };
+            }
         }
 
+        //ADD A PRODUCT
+        static void AddProduct()
+        {
+            _name = ReadString("Enter name: ", true);
+            _price = ReadDecimal("Enter price: ", 0);
+            _description = ReadString("Enter description (if any): ", false);
+        }
+
+        //READ STRING (FOR PRODUCT ADD)
+        private static string ReadString( string message, bool isRequired )
+        {
+            do
+            {
+                Console.Write(message);
+                string value = Console.ReadLine();
+                    
+                if (!isRequired || value != "")
+                    return value;
+
+                Console.WriteLine("You cannot leave this entry blank");
+            } while (true);
+        }
+
+        //READ DECIMAL (FOR PRODUCT ADD)
+        private static decimal ReadDecimal( string message, decimal minValue )
+        {
+            do
+            {
+                Console.Write(message);
+                string value = Console.ReadLine();
+
+                
+                if (Decimal.TryParse(value, out decimal result))
+                {
+                    if (result >= minValue)
+                        return result;
+                };
+
+                Console.WriteLine("Must be a number and >= {0}", minValue);
+            } while (true);
+        }
+
+        //LIST PRODUCTS
+        private static void ListProducts()
+        {
+            //CHECK FOR PRODUCT
+            if (_name != null && _name != "")
+            {
+                Console.WriteLine(_name);
+                Console.WriteLine(_price);
+                Console.WriteLine(_description);
+            } else
+                Console.WriteLine("No products"); 
+        }
+
+        //PRODUCT DATA
+        static string _name;
+        static decimal _price;
+        static string _description;
+   
+        //MENU DISPLAY
+        private static char DisplayMenu()
+        {
+            //MENU OPTIONS
+            do
+            {
+                Console.WriteLine("(L)ist Products");
+                Console.WriteLine("(A)dd Products");
+                Console.WriteLine("(Q)uit");
+
+                string input = Console.ReadLine();
+
+                if (input == "L" || input == "l")
+                    return input[0];
+                else if (input == "A" || input == "a")
+                    return input[0];
+                else if (input == "Q" || input == "q")
+                    return input[0];
+               
+                Console.WriteLine("Please choose a valid option");
+            } while (true);   
+               
+        }
+        
+        //OTHER
         static void PlayingWithPrimitives ()
         {
             //Primitive
